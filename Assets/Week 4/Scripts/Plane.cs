a378a68b22f2e3e5b18434569ec630451c66f514
auburn <astefanadon@gmail.com>
2024-02-01 14:42:56 -0500

    Week 4 Task 2 done with plane landing and disapearing being completed.

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -8,16 +8,73 @@ public class Plane : MonoBehaviour
     public float newPointThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
-
+    Rigidbody2D rigidbody;
+    Vector2 currentPosition;
+    public float speed = 1;
+    public AnimationCurve landing;
+    float timerValue;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0,transform.position);
     }
 
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if (points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigidbody.rotation = -angle;
+        }
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up * speed * Time.deltaTime);
 
+
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space)) 
+        {
+            timerValue += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(timerValue);
+
+            
+            if(transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+
+            }
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+
+        }
+
+
+
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if (Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 2; i ++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                    
+
+                }
+                lineRenderer.positionCount--;
+
+            }
+        }
+        
+    }
 
     private void OnMouseDown()
     {
