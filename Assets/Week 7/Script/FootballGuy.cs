ba5626c866d4ee54ed152d4bdecdaa8f6bd11854
auburn <astefanadon@gmail.com>
2024-02-22 14:21:53 -0500

    Task1b complete (football/soccer script and prefab made)

---
+++ Assets/Week 7/Script/FootballGuy.cs
@@ -0,0 +1,45 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootballGuy : MonoBehaviour
+{
+    public SpriteRenderer Spritething;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spritething = GetComponent<SpriteRenderer>();
+        Selected(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    private void OnMouseDown()
+    {
+        Selected(true);
+    }
+
+    public void Selected(bool IsSelected)
+    {
+        if (IsSelected)
+        {
+            Spritething.color = Color.white;
+        }
+        else
+        {
+            Spritething.color = Color.red;
+        }
+
+    }
+
+
+}
