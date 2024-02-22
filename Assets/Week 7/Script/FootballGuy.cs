e39d965e7d20c21ec6438d3963364e1ca7a6bf73
auburn <astefanadon@gmail.com>
2024-02-22 15:34:36 -0500

    Task 2 Player Movement and charge complete

--- Assets/Week 7/Script/FootballGuy.cs
+++ Assets/Week 7/Script/FootballGuy.cs
@@ -7,22 +7,22 @@ using UnityEngine;
 public class FootballGuy : MonoBehaviour
 {
     public SpriteRenderer Spritething;
+    Rigidbody2D Rigi;
+    public float speed = 5000;
 
-
+    
 
     // Start is called before the first frame update
     void Start()
     {
         Spritething = GetComponent<SpriteRenderer>();
         Selected(false);
+        Rigi = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-         
-    }
+    
 
     private void OnMouseDown()
     {
@@ -39,6 +39,14 @@ public class FootballGuy : MonoBehaviour
         {
             Spritething.color = Color.red;
         }
+
+    }
+
+
+    public void Move(Vector2 direction)
+    {
+        Rigi.AddForce(direction * speed);
+        Debug.Log("eii");
 
     }
 
--- Assets/Week 7/Script/TheController.cs
+++ Assets/Week 7/Script/TheController.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 
 public class TheController : MonoBehaviour
 {
+    public Slider ChargeSlider;
+    float charge;
+    public float maxCharge = 1;
+    Vector2 direction;
+
     public static FootballGuy SelectedPlayer {  get; private set; }
     public static void SetSelectedPlayer(FootballGuy Player)
     {
@@ -18,6 +23,46 @@ public class TheController : MonoBehaviour
     }
 
 
+    private void FixedUpdate()
+    {
+        if(direction != Vector2.zero)
+        {
+            SelectedPlayer.Move(direction);
+            Debug.Log(direction);
+            direction = Vector2.zero;
+            charge = 0;
+            ChargeSlider.value = charge;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (SelectedPlayer == null) return;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            charge = 0;
+            direction = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            charge += Time.deltaTime;
+            charge = Mathf.Clamp(charge, 0, maxCharge);
+            ChargeSlider.value = charge;
+
+
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)SelectedPlayer.transform.position).normalized * charge;
+        }
+    }
+
+
+    
 
 
 
